72ae634f942f3c17ef2f374256a26744c22c3c60
megazear7 <alexlockhart7@gmail.com>
2014-06-18 11:32:48 -0400

    added some more links for dm stuff and changed basic route

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => "encounters#index"
+  root :to => "characters#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
