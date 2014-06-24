7acf2c5a00a13c846b21284949da3a00cd4d1896
megazear7 <alexlockhart7@gmail.com>
2014-06-24 14:54:01 -0400

    added to the character show page and abilities scores should list by range now

--- app/models/ability_score.rb
+++ app/models/ability_score.rb
@@ -8,4 +8,11 @@ class AbilityScore < ActiveRecord::Base
     nil
   end
 
+  def self.find_ability_mod ability, value, mod
+    where(ability: ability).each do |ability_score|
+      return ability_score.send(mod) if ability_score.value_range.cover? value
+    end
+    0
+  end
+
 end
