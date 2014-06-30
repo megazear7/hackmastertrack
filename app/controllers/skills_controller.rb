49d7ce31aacae7aa9a56e3afc96e82550ce7838e
megazear7 <alexlockhart7@gmail.com>
2014-06-30 11:33:14 -0400

    added to skill controller and add skill show page

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -69,6 +69,12 @@ class SkillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def skill_params
-      params[:skill]
+      params.require(:skill).permit(
+        :name,
+        :description,
+        :bp_cost,
+        :main_attr,
+        :other_attr
+      )
     end
 end
