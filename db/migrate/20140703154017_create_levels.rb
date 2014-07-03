59358f61d0283be6f40179cc71a03b49b4f02066
James M. Rogers <JamesMRogers@gmail.com>
2014-07-03 12:37:55 -0400

    Added level mods to project

--- db/migrate/20140703154017_create_levels.rb
+++ db/migrate/20140703154017_create_levels.rb
@@ -1,13 +1,13 @@
 class CreateLevels < ActiveRecord::Migration
   def change
     create_table :levels do |t|
-      t.integer level_value
-      t.integer init_mod
-      t.integer attack_mod
-      t.integer speed_mod
-      t.integer spell_points
+      t.integer :level_value
+      t.integer :init_mod
+      t.integer :attack_mod
+      t.integer :speed_mod
+      t.integer :spell_points
 
-      t.integer character_class_id
+      t.integer :character_class_id
 
       t.timestamps
     end
