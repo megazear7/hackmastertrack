b6f031b39429108c670cdf7cab8b551d979eab27
megazear7 <alexlockhart7@gmail.com>
2014-07-08 07:08:11 -0400

    added weapons back into prof dropdown as weapon types changed

--- app/helpers/proficiencies_helper.rb
+++ app/helpers/proficiencies_helper.rb
@@ -1,6 +1,8 @@
 module ProficienciesHelper
   def prof_list
-    list =      Item.where(item_type: "weapon").pluck(:name, :skill_level).map {|n, s| n + " (" + (s ? s.titleize : "-") + ")"}
+    list =      Item.where(item_type: "melee").pluck(:name, :skill_level).map {|n, s| n + " (" + (s ? s.titleize : "-") + ")"}
+    list =      Item.where(item_type: "ranged").pluck(:name, :skill_level).map {|n, s| n + " (" + (s ? s.titleize : "-") + ")"}
+    list =      Item.where(item_type: "polearm").pluck(:name, :skill_level).map {|n, s| n + " (" + (s ? s.titleize : "-") + ")"}
     list.concat Item.where(item_type: "armor").pluck(:name, :armor_type).map   {|n, s| n + " (" + (s ? s.titleize : "-") + ")"}
     list.concat Item.where(item_type: "shield").pluck(:name, :shield_size).map {|n, s| n + " (" + (s ? s.titleize : "-") + ")"}
     list
