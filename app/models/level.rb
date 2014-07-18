b77ec0a243461bc4e1b47e1e5101e6134a12ffa7
James M. Rogers <JamesMRogers@gmail.com>
2014-07-18 18:32:38 -0400

    making changes for level and race to the pdf mappings.

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,3 +1,19 @@
 class Level < ActiveRecord::Base
   belongs_to :character_class
+
+  def self.find_mod theclass, level, mod
+    where(character_class_id: theclass).each do |ability_score|
+      # TODO query the database instead of looping
+      if ability_score.level_value == level
+        mod_val = ability_score.send(mod)
+        if mod_val.nil?
+          return 0
+        else
+          return mod_val
+        end
+      end
+    end
+    0
+  end
+
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -1,3 +1,16 @@
 class Race < ActiveRecord::Base
   has_many :bp_cost_by_race_classes
+
+  def self.find_mod race, mod
+    where(name: race).each do |thisrace|
+      # TODO query the database instead of looping
+      mod_val = thisrace.send(mod)
+      if mod_val.nil?
+        return 0
+      else
+        return mod_val
+      end
+    end
+    0
+  end
 end
