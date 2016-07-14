0e58f29615e695b41907e5073ed002ffe57f4a73
Alex Lockhart <alexlockhart7@gmail.com>
2016-07-14 14:09:16 -0400

    added random number generator for stats

--- app/assets/javascripts/application/generic_helpers.js
+++ app/assets/javascripts/application/generic_helpers.js
@@ -13,6 +13,10 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
 };
 
+function random(min,max) {
+        return Math.floor(Math.random()*(max-min+1)+min);
+}
+
 $(document).ready(function() {
   $(".show-on-hover").each(function(index, element) {
     var show = $(element).data("show");
@@ -24,18 +28,16 @@ $(document).ready(function() {
       $("."+show).hide();
     });
   });
-});
 
-$(document).ready(function() {
-  subNav = $(".sub-nav");
-  active = document.body.scrollTop > 50
-  first = true
+  var subNav = $(".sub-nav");
+  var active = document.body.scrollTop > 50
+  var first = true
   $(window).scroll(function(e){
     if ($(window).width() > 992) { 
         if ((! active && document.body.scrollTop > 50) || first) {
           first = false;
           active = true;
-          placeholder = subNav.clone().empty().css("height", subNav.height()+"px").addClass("subnav-placeholder");
+          var placeholder = subNav.clone().empty().css("height", subNav.height()+"px").addClass("subnav-placeholder");
           subNav.after(placeholder);
           subNav.addClass("active");
         } else if ((active && document.body.scrollTop < 50) || first) {
@@ -48,17 +50,41 @@ $(document).ready(function() {
   });
 
   $(".click-link").click(function(e) {
-    clickableElement = $(e.target).closest(".click-link");
+    var clickableElement = $(e.target).closest(".click-link");
     if (clickableElement.length == 0) {
       clickableElement = $(e.target)
     }
-    clickId = clickableElement.data("click")
+    var clickId = clickableElement.data("click")
     document.location = $("#"+clickId).attr("href");
   });
 
   $(".selectable").click(function(e) {
-    group = $(e.target).data("selectable-group");
+    var group = $(e.target).data("selectable-group");
     $("."+group).removeClass("selected");
     $(e.target).addClass("selected");
+  });
+
+  $(".random").click(function(e) {
+    e.preventDefault;
+    var button = $(e.target).closest(".click-link");
+    if (button.length == 0) {
+      button = $(e.target)
+    }
+    if (parseInt(button.data("count")) < parseInt(button.data("limit"))) {
+      count = button.data("count");
+      button.data("count", count+1);
+      var id = button.data("target");
+      var whole = random(1, 6) + random(1, 6) + random(1, 6)
+      var value = whole + "." + random(0,99)
+      $("#"+id).val(value);
+    } else {
+      limit = parseInt(button.data("limit"));
+      if (limit == 1) {
+        alert("Only one roll allowed");
+      } else {
+        alert("Only "+limit+" rolls allowed");
+      }
+    }
+    return false;
   });
 });
