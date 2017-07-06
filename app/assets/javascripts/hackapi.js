371e1541161436bd2ec74a04a623f818a646b4a0
megazear7 <alexlockhart7@gmail.com>
2017-07-06 08:13:16 -0400

    Added client side API caching so that the React components wouldn't have to worry about holding on to API data.

--- app/assets/javascripts/hackapi.js
+++ app/assets/javascripts/hackapi.js
@@ -1,40 +1,64 @@
 (function() {
     window.HackAPI = { };
 
-    var HackRequest = function(path) {
-        var self = this;
+    // Max age is given in seconds
+    var cache = { maxAge: 5 };
 
-        $.get(path)
-        .done(function(result) {
-            if (Array.isArray(result)) {
-                $.each(results, function(result) {
+    var HackRequest = function(category, id) {
+        var self = this;
+        var path = jsonPath(category, id);
+
+        var isCached = cache[category] && cache[category][id] && cache[category][id].expire > new Date();
+        var cachedResponse;
+
+        if (cache[category] && cache[category][id] && cache[category][id].expire > new Date()) {
+            cachedResponse = cache[category][id].cachedResponse;
+        } else {
+            $.get(path)
+            .done(function(result) {
+
+                if (typeof cache[category] === "undefined") {
+                    cache[category] = { };
+                }
+
+                var expire = new Date();
+                expire.setSeconds(expire.getSeconds() + cache.maxAge);
+                cache[category][id] = {expire: expire, cachedResponse: result};
+
+                if (Array.isArray(result)) {
+                    $.each(results, function(result) {
+                        if (typeof self.doneCallback === "function") {
+                            self.doneCallback(result);
+                        }
+                    });
+                } else {
                     if (typeof self.doneCallback === "function") {
                         self.doneCallback(result);
                     }
-                });
-            } else {
-                if (typeof self.doneCallback === "function") {
-                    self.doneCallback(result);
                 }
-            }
 
-            if (typeof self.andThenCallback === "function") {
-                self.andThenCallback();
-            }
-        })
-        .fail(function(error) {
-            if (typeof self.failCallback === "function") {
-                self.failCallback(error);
-            }
-        })
-        .always(function() {
-            if (typeof self.alwaysCallback === "function") {
-                self.alwaysCallback();
-            }
-        });
+                if (typeof self.andThenCallback === "function") {
+                    self.andThenCallback();
+                }
+            })
+            .fail(function(error) {
+                if (typeof self.failCallback === "function") {
+                    self.failCallback(error);
+                }
+            })
+            .always(function() {
+                if (typeof self.alwaysCallback === "function") {
+                    self.alwaysCallback();
+                }
+            });
+        }
 
         self.done = function(callback) {
             self.doneCallback = callback;
+
+            if (isCached) {
+                self.doneCallback(cachedResponse);
+            }
 
             return self;
         };
@@ -48,11 +72,23 @@
         self.always = function(callback) {
             self.alwaysCallback = callback;
 
+            if (isCached) {
+                self.alwaysCallback();
+            }
+
             return self;
         };
 
         self.each = function(callback) {
             self.doneCallback = callback;
+
+            if (isCached) {
+                $.each(cachedResponse, function(result) {
+                    if (typeof self.doneCallback === "function") {
+                        self.doneCallback(result);
+                    }
+                });
+            }
 
             return self;
         };
@@ -60,20 +96,24 @@
         self.andThen = function(callback) {
             self.andThenCallback = callback;
 
+            if (isCached) {
+                self.andThenCallback();
+            }
+
             return self;
         };
     };
     
     window.HackAPI.characters = {
         find: function(ids) {
-            return new HackRequest(path("characters", ids));
+            return new HackRequest("characters", ids);
         },
         findOne: function(id) {
-            return new HackRequest(path("characters", id));
+            return new HackRequest("characters", id);
         }
     };
     
-    function path() {
+    function jsonPath() {
         return "/" + Array.from(arguments).join("/") + ".json"
     }
 })()
